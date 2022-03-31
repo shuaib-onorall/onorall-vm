dca41b350c07334e155e9f4e8e03a2814c0edd26
amitsingh <amitsofficial1998@gmail.com>
2022-03-31 11:29:26 +0530

    a

--- backend/settings.py
+++ backend/settings.py
@@ -123,7 +123,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("https://cretskill-backend.herokuapp.com/", 6379)],
         },
     },
     'ROUTING': 'ws.routing.application',
