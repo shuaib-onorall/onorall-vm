3c90cb98316dae831a2e382bd1caacf3668f967d
shuaib <shuaib.onorall2k2@gmail.com>
2022-03-31 21:47:46 -0700

    n

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -317,17 +317,26 @@ class ReplySerializer(serializers.ModelSerializer):
         
 
 class CommentSerializer(serializers.ModelSerializer):
-    #creater=serializers.SerializerMethodField('video_owner_func')
-    
-    # def video_owner_func(self,obj):
-    #     obj=sign.objects.get(id = obj.video_id.user_id.id)
-    #     return obj.id
+
+    user_name = serializers.SerializerMethodField('user_name_func')
+    is_creater  =  serializers.SerializerMethodField('is_creater_func')
+
+    def user_name_func(self,obj):
+        obj=sign.objects.get(id = obj.user_id.id)
+        return obj.name
+
+    def is_creater_func(self , obj):
+        creater_obj = sign.objects.get( id = obj.video_id.user_id.id )
+        comment_user_obj = sign.objects.get( id = obj.user_id )
+        if creater_obj.id == comment_user_obj.id:
+            return True
+        return False
 
 
     class Meta:
         model = Commentss
-        fields = ['created_time' , "comment_text" , "user_id"  , "replies" , "video_id" , "likes_on_comment" , "dis_likes_on_comment"] #__all__"
-        depth = 1
+        fields = ['id' , "parent" ,  'is_creater' , "video_id" ,  "likes_on_comment" , "dis_likes_on_comment" , "comment_text" ,  "created_time" , "user_id"  ,  "user_name" , 'like_active' , 'dislike_active'  ] #__all__"
+        #depth = 1
 
 
 class timelineSerializer(serializers.ModelSerializer):
